645addbed79a28d95fb52bbd76c1a87d17fa156a
keqingrong <keqingrong1992@gmail.com>
2015-10-31 10:26:35 +0800

    Update dom-without-jquery.js

--- javascript/dom-without-jquery.js
+++ javascript/dom-without-jquery.js
@@ -12,14 +12,34 @@ document.getElementById("MyElement").classList.toggle('class');
 */
 
 function hasClass(element, class) {
-    return element.className.match(new RegExp('(\\s|^)' + class + '(\\s|$)'));
+  return element.className.match(new RegExp('(\\s|^)' + class + '(\\s|$)'));
 }
+
 function addClass(element, class) {
-    if (!hasClass(element, class)) element.className += " " + class;
+  if (!hasClass(element, class)) element.className += " " + class;
 }
+
 function removeClass(element, class) {
-    if (hasClass(element, class)) {
-        var reg = new RegExp('(\\s|^)' + class + '(\\s|$)');
-        element.className = element.className.replace(reg, ' ');
-    }
+  if (hasClass(element, class)) {
+    var reg = new RegExp('(\\s|^)' + class + '(\\s|$)');
+    element.className = element.className.replace(reg, ' ');
+  }
+}
+
+function replaceClass(element, oldClass, newClass){
+  if(hasClass(element, oldClass)){
+    removeClass(element, oldClass);
+    addClass(element, newClass);
+  }
+  return;
+}
+
+function toggleClass(element, class1, class2){
+  if(hasClass(element, class1)){
+    replaceClass(element, class1, class2);
+  }else if(hasClass(element, class2)){
+    replaceClass(element, class2, class1);
+  }else{
+    addClass(element, class1);
+  }
 }
