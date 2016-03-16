aa19d2e34b06e515208894d9cdaf83a33de92ebe
Qingrong <keqingrong1992@gmail.com>
2016-03-17 00:32:14 +0800

    Update xhr.js

--- javascript/xhr.js
+++ javascript/xhr.js
@@ -2,7 +2,8 @@
 * XMLHttpRequest
 */
 
-var request = new XMLHttpRequest(); //
+/* state: unsent, opened, headers received, loading, and done */
+var request = new XMLHttpRequest();
 console.log('UNSENT', request.readyState); // readyState will be 0
 
 request.open("GET", "filename", true);
@@ -16,10 +17,20 @@ request.onload = function () {
     console.log('DONE', request.readyState); // readyState will be 4
 };
 
-request.onreadystatechange = function() {
-    if (request.readyState === 4 && request.status === 200) {
-       // Action to be performed when the document is read;
-    }
-};
-
 request.send();
+
+/* event: readystatechange, progress, abort, error, load, timeout, and loadend */
+/* event handlers */
+var request = new XMLHttpRequest();
+request.onreadystatechange = function() {};
+request.open("GET", "filename", true);
+request.onprogress = function () {};
+request.onabort = function () {};
+request.onerror = function () {};
+request.onload = function () {};
+request.ontimeout = function () {};
+request.onloadend = function () {};
+request.send();
+
+
+
